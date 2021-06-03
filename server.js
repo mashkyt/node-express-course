02b39983722f3a7e5cee55d6e2b59fa0858486c4
rylonm <rylon@umich.edu>
2021-06-03 20:05:24 +0800

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express'); // search for module express
 const app = express(); // create app as an instance with express constructor
 
+const mockUserData = [
+    {name: 'Mack'},
+    {name: 'Jill'}
+];
+
+app.get('/users', function(req, res){
+    res.json({
+        success: true,
+        message: 'Successfully got this. Good job!',
+        users: mockUserData
+    })
+});
+
 app.listen(8000, function(){
     console.log("server is running");
 }); // listen to port 8000
+
+
