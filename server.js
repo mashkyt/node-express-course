c08adf86b0f2585ad223bd4808f2ed459bed1df9
rylonm <rylon@umich.edu>
2021-06-03 20:21:01 +0800

    add dynamic GET route

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ app.get('/users', function(req, res){
     })
 });
 
+app.get('/users/:id', function(req, res){
+    console.log(req.params.id);
+    res.json({
+        success: true,
+        message: 'Successfully got this. Good job!',
+        users: req.params.id
+    })
+});
+
 app.listen(8000, function(){
     console.log("server is running");
 }); // listen to port 8000
